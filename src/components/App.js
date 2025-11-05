91a837b9de6a30f5b48672916804aef17f438497
Aditya Singh <103933450+LINU00@users.noreply.github.com>
2025-11-06 01:01:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,13 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import UseMemo from "./UseMemo";
+import ReactMemo from "./ReactMemo";
 
+export default function App() {
+  return (
+    <div id="main" className="container">
+      <h1>Task Management App (React Memo)</h1>
+      <UseMemo />
+      <ReactMemo />
+    </div>
+  );
+}
